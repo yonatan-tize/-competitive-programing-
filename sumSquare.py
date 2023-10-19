c2aa2b08c00ac05a31e071d01b5384d8ac0e36df
yonatan-tize <121636862+yonatan-tize@users.noreply.github.com>
2023-10-19 07:48:54 +0300

    Add files via upload

---
+++ sumSquare.py
@@ -0,0 +1,15 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left, right = 0, int(c**0.5)  
+
+        while left <= right:
+
+            curr_sum = left**2 + right**2
+            
+            if curr_sum == c:
+                return True
+            elif curr_sum < c:
+                left += 1
+            else:
+                right -= 1
+        return False

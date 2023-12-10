0061b6853d0defde841077c236a60893feb2fa4d
yonatan-tize <121636862+yonatan-tize@users.noreply.github.com>
2023-12-10 23:33:48 +0300

    Added solution for Missing Number

--- onboarding/missing-number.py
+++ onboarding/missing-number.py
@@ -1,5 +1,34 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        i = 0
+        l = len(nums)
+        while i < l:
+            if nums[i] < l and i != nums[i]:
+                temp = nums[i]
+                nums[i] = nums[temp]
+                nums[temp] = temp
+            else:
+                i += 1
+        for j in range(len(nums)):
+            if j != nums[j]:
+                return j
+        return len(nums)
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         nums.sort()
         for i,num in enumerate(nums):
             if num != i:

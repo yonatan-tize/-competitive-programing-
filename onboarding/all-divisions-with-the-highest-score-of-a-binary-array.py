8d4a1250434882d378d4933ba733c5e1d53f6a81
yonatan-tize <121636862+yonatan-tize@users.noreply.github.com>
2023-12-20 10:18:57 +0300

    Added solution for All Divisions With the Highest Score of a Binary Array

---
+++ onboarding/all-divisions-with-the-highest-score-of-a-binary-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxScoreIndices(self, nums: List[int]) -> List[int]:
+        count1 = nums.count(1)
+        count0 = 0
+        store = defaultdict(list)
+        store[count1].append(0) 
+        ans = count1
+
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                count0 += 1
+            else:
+                count1 -= 1
+            divScore = count0 + count1
+            store[divScore].append(i+1) 
+            ans = max(ans, divScore)
+            
+        return store[ans]
+
+

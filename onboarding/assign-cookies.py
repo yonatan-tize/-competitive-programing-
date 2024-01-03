859fb2e8ef848c9fca89b2878758e3febd2563cd
yonatan-tize <121636862+yonatan-tize@users.noreply.github.com>
2024-01-03 17:15:57 +0300

    Added solution for Assign Cookies

---
+++ onboarding/assign-cookies.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        ans = 0
+        i = 0
+        j = 0
+        while i < len(g) and j < len(s):
+            if g[i] <= s[j]:
+                ans += 1
+                i += 1
+            j += 1
+        return ans
+          

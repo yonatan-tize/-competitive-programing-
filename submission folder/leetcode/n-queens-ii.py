524823767a1f083d397930c4cd3c971e95deb508
yonatan-tize <121636862+yonatan-tize@users.noreply.github.com>
2024-03-03 16:29:02 +0300

    Add solution for N-Queens II

---
+++ submission folder/leetcode/n-queens-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def is_safe(board, row, col):
+            for prev_row in range(row):
+                if board[prev_row][col] == 'Q':
+                    return False
+                if col - (row - prev_row) >= 0 and board[prev_row][col - (row - prev_row)] == 'Q':
+                    return False
+                if col + (row - prev_row) < n and board[prev_row][col + (row - prev_row)] == 'Q':
+                    return False
+            return True
+        self.ans = 0
+        
+        def backtrack(row):
+            if row == n:
+                # solutions.append([''.join(row) for row in board])
+                self.ans += 1
+                return
+            for col in range(n):
+                if is_safe(board, row, col):
+                    board[row][col] = 'Q'
+                    backtrack(row + 1)
+                    board[row][col] = '.'
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        solutions = []
+        backtrack(0)
+        return self.ans
